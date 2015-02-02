2abf315717fbc5ae60f6173b9ce2b06d8130c423
Jesus Maldonado <jesusmaldonado317@gmail.com>
2015-02-01 21:23:37 -0500

    recursion exercises

---
+++ practice_problems/sum_rec.rb
@@ -0,0 +1,24 @@
+def sum_rec(arr)
+  return arr[0] if arr.size == 1
+  value = arr.shift
+  value + sum_rec(arr)
+end
+
+
+#the disadvantage with this si that the array is destructively sorted
+
+def sum_rec(arr)
+  return arr[0] if arr.size == 1
+  arr[0] + sum_rec(arr.drop(1))
+end
+
+#O(n) time, with O(n) memory
+#initiation
+#add the first element to a recursive call
+#propagation
+#keep recursing until the element is size 1
+#termination
+#once we hit 0, the recursive calls return values to subsequent calls, which are added together
+
+
+#this is a little beter because the array is not destructively recursed through
