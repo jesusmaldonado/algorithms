379316dd6cafcb19a3caca744911de348fbfca66
Jesus Maldonado <jesusmaldonado317@gmail.com>
2015-02-19 02:45:35 -0500

    moving zeros

--- problems/move_zeros.rb
+++ problems/move_zeros.rb
@@ -1,13 +1,19 @@
 def move_zeros(array)
-
+	##keep track of everything we flip
+	zero_place = 0
+	array_length = array.length
+	##"lazy" approach, keep track of the last zero_place switch.
 	array.each_with_index do |value, index|
-			if value == 0 && array[index + 1]
-				array[index] = array[index + 1]
-				array.delete_at(index + 1)
-				array << 0
+		##counter will lag behind the minute there is a zero, and will allow values to bubble up
+			if value != 0
+				array[zero_place] = array[index]
+				##there is no zero here, but there may be at the next
+				zero_places += 1
 			end
+	end
+	#well we know where to zeros are
+	(zero_places...array.length).each do |zero_index|
+		array[zero_index] = 0
 	end
 	array
 end
-
-p move_zeros([1, 2, 0, 3, 4, 0, 5, 6, 0])
