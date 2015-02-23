fdcb46302b84bcb4d6cbe0761cd37deced1c658e
Jesus Maldonado <jesusmaldonado317@gmail.com>
2015-02-23 01:30:18 -0500

    adds more to k_consensus

--- problems/k_consensus.rb
+++ problems/k_consensus.rb
@@ -5,19 +5,26 @@ def k_consensus(outer_array, k3)
 	raise "consensus not possible" if k3 > outer_array.size
 	#brute force, nested hash
 	hash = Hash.new { |k, v| k[v] = Set.new}
-
+	output = []
 	outer_array.each_with_index do |array, index|
 		array.each do |val|
 			hash[index] << val
 		end
 	end
+
 	unique_counts = Hash.new { |k, v| k[v] = 0}
 	hash.each do |array_index, set|
 		set.each do |number|
 			unique_counts[number] += 1
 		end
+		hash[array_index] = nil
 	end
-	unique_counts.select{|k,v| v >= k3}.keys
+	output = []
+
+	unique_counts.each do |k, v|
+		output << k if v >= k3
+	end
+	output
 end
 
 p k_consensus([[1,2,2,3,45], [1,2,3,45], [1,2,6,7]], 2)
